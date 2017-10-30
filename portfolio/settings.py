15889857b279ad1ef92e4d95ec96e8d734102c27
Vlad <vlad.moroshan@gmail.com>
2017-10-30 19:36:53 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,9 +124,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # For sending emails
 EMAIL_USE_TLS = True 
