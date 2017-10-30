bcfd735fdf9e97eb10a2a2ca9e92e5de78c27713
Vlad <vlad.moroshan@gmail.com>
2017-10-30 19:09:56 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # For sending emails
 EMAIL_USE_TLS = True 
