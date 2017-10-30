8428dd1b88769c3688fe9228c540fa3614ffd53a
Vlad <vlad.moroshan@gmail.com>
2017-10-30 18:46:00 +0200

    Update views.py

--- mainpage/views.py
+++ mainpage/views.py
@@ -31,6 +31,6 @@ class ProjectListAndFormView(SuccessMessageMixin, ListView, FormView):
             cd['message'],
             cd.get('email', 'noreply@example.com'),
             ['vlad.moroshan@gmail.com'],
-            connection=con
+            fail_silently=False
         )
         return super(ProjectListAndFormView, self).form_valid(form)
