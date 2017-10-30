eebd7280ea8a9dd49617fef79d885b4c0fb998f6
Vlad <vlad.moroshan@gmail.com>
2017-10-30 18:43:25 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# For sending emails
+EMAIL_USE_TLS = True 
+EMAIL_HOST ='smtp.gmail.com' 
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = '*********'
+EMAIL_PORT = 587
