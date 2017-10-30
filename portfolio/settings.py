718df7c8f3f6b0cdbf55072c8f9ff326b7ef6976
Vlad <vlad.moroshan@gmail.com>
2017-10-30 19:17:57 +0200

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,9 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS =  ['*']
 
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -123,6 +120,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_DIR, 'static'),
+]
 
 # For sending emails
 EMAIL_USE_TLS = True 
