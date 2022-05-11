2cc07ee3f36846f58699c6690957f1146c389972
vmoroshan <vlad.moroshan@gmail.com>
2022-05-11 23:55:16 +0300

    Remove depreceted

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn portfolio.wsgi --log-file -
--- mainpage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .views import ProjectListAndFormView
-
-urlpatterns = [
-    url(r'^$', ProjectListAndFormView.as_view(), name='main')
-]
